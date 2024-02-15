c84b6d9cf095be6b37ad0be7cf0bf072840cb432
Melk de Sousa <melk.sousap@gmail.com>
2024-02-15 09:49:09 -0300

    refactor: config cookies

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,6 +2,8 @@ import z from "zod";
 
 export const EnvSchema = z.object({
 	DATABASE_URL: z.string(),
+	SESSION_SECRET: z.string().min(16),
+	NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
 export const envs = EnvSchema.parse(process.env);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import "express-async-errors";
+import session from "express-session";
 import morgan from "morgan";
 import z from "zod";
+import { envs } from "./config/env";
 import "./database/mongo/connection";
 import { zodErrorHandler } from "./express/middlewares";
 
@@ -9,8 +11,27 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("common"));
+app.use(
+	session({
+		secret: envs.SESSION_SECRET,
+		cookie: {
+			secure: envs.NODE_ENV === "production",
+			httpOnly: true,
+			// maxAge: 1000 * 60 * 60 * 24, // 1 day
+		},
+		resave: false,
+		saveUninitialized: false,
+	}),
+);
 
 app.route("/products").post((req, res) => {
+	const cookieConfigs = {
+		path: "/",
+		// maxAge: 60 * 60 * 24 * 30, // 30 days
+		signed: true,
+		httpOnly: true,
+	};
+
 	const bodySchema = z.object({
 		title: z.string(),
 		description: z.string(),
@@ -18,9 +39,21 @@ app.route("/products").post((req, res) => {
 		category: z.string(),
 	});
 
+	const cookiesSchema = z.object({
+		ownerId: z.string().uuid().optional(),
+	});
+
+	const cookies = cookiesSchema.parse(req.session);
+
+	if (!cookies.ownerId) {
+		cookies.ownerId = crypto.randomUUID();
+		Object.assign(req.session, cookies);
+		Object.assign(req.session.cookie, cookieConfigs);
+	}
+
 	const body = bodySchema.parse(req.body);
 
-	res.status(201).send(body);
+	res.status(201).send({ ...body, ...cookies });
 });
 
 app.use(zodErrorHandler);
