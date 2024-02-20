eeb8f3b88ba963600996d67c5ca448b1e0bc61e9
Melk de Sousa <melk.sousap@gmail.com>
2024-02-19 23:02:38 -0300

    feat: add list user controller and endpoint

---
+++ src/express/controllers/list-users.ts
@@ -0,0 +1,70 @@
+import { UserModel } from "@/database/mongo/models-and-schemas/User";
+import { toUserOutput } from "@/dtos/user";
+import { limitSchema, pageSchema } from "@/schemas/query";
+import { RequestHandler } from "express";
+import z from "zod";
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get a list of users
+ *     description: Retrieve a paginated list of users
+ *     parameters:
+ *       - name: page
+ *         in: query
+ *         description: The page number to retrieve
+ *         required: false
+ *         schema:
+ *           type: integer
+ *       - name: limit
+ *         in: query
+ *         description: The number of items per page
+ *         required: false
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       '200':
+ *         description: A JSON array of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/CreateUserResponse'
+ *                 totalPages:
+ *                   type: integer
+ *                 currentPage:
+ *                   type: integer
+ */
+export const listUsers: RequestHandler = async (req, res) => {
+	const querySchema = z.object({
+		page: pageSchema,
+		limit: limitSchema,
+	});
+
+	const { limit, page } = querySchema.parse(req.query);
+	console.log(
+		"🚀 ~ constlistUsers:RequestHandler= ~ limit, page:",
+		limit,
+		page,
+	);
+
+	const users = await UserModel.find()
+		.limit(limit * 1)
+		.skip((page - 1) * limit)
+		.exec();
+
+	const totalUsers = await UserModel.countDocuments();
+
+	return res
+		.json({
+			users: users.map(toUserOutput),
+			totalPages: Math.ceil(totalUsers / limit),
+			currentPage: page,
+		})
+		.status(200);
+};
--- src/express/routes/users.ts
+++ src/express/routes/users.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createUser } from "../controllers/create-user";
+import { listUsers } from "../controllers/list-users";
 
 const usersRouter = Router();
 
-usersRouter.route("/").post(createUser);
+usersRouter.route("/").post(createUser).get(listUsers);
 
 export { usersRouter };
