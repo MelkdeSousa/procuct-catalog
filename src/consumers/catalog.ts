5c2ea2f81cd7c14694c5bf24ee6dc10d0ffe5b18
Melk de Sousa <melk.sousap@gmail.com>
2024-02-26 09:16:52 -0300

    feat: consume change on products and store in catalog in json file

--- src/consumers/catalog.ts
+++ src/consumers/catalog.ts
@@ -1,15 +1,52 @@
 import "@/database/mongo/connection";
-import { ProductModel } from "@/database/mongo/models-and-schemas/Product";
+import {
+	Category,
+	CategoryModel,
+} from "@/database/mongo/models-and-schemas/Category";
+import { Product } from "@/database/mongo/models-and-schemas/Product";
+import { UserModel } from "@/database/mongo/models-and-schemas/User";
 import { queues } from "@/services/queue";
+import { writeFile } from "fs/promises";
+import path from "path";
 
-queues["catalog-emit"].process(async (job) => {
-	return ProductModel.find({ ownerId: job.data })
-		.then((products) => {
-			console.log({
-				ownerId: job.data,
-				time: new Date().toISOString(),
-				total: products.length,
-			});
-		})
-		.catch(console.error);
+queues["catalog-emit"].process(async ({ data: ownerId }) => {
+	const owner = await UserModel.findById(ownerId);
+
+	const products = (await CategoryModel.aggregate([
+		{
+			$match: {
+				ownerId: owner?._id,
+			},
+		},
+		{
+			$lookup: {
+				from: "products",
+				localField: "_id",
+				foreignField: "categoryId",
+				as: "products",
+			},
+		},
+	])) as (Category & { products: Product[] })[];
+
+	const catalog = {
+		owner: owner?.name,
+		catalog: products.map((p) => ({
+			category_title: p.title,
+			category_description: p.description,
+			items: p.products.map((p) => ({
+				tile: p.title,
+				description: p.description,
+				price: p.price,
+			})),
+		})),
+	};
+
+	await writeFile(
+		path.join(process.cwd(), "tmp", `${ownerId}-catalog.json`),
+		JSON.stringify(catalog, null, 2),
+	);
+	// criar um arquivo json com as categorias e os produtos
+	// publicar o arquivo no bucket
+
+	return;
 });
