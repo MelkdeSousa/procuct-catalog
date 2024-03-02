f4160bc11b2a860efcb852a35461f1fc63857364
Melk de Sousa <melk.sousap@gmail.com>
2024-03-02 14:08:56 -0300

    feat: expose delete product and get category endpoints

--- src/express/routes/categories.ts
+++ src/express/routes/categories.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createCategory } from "../controllers/category/create-category";
+import { getCategory } from "../controllers/category/get-category";
 import { listCategories } from "../controllers/category/list-categories";
 import { updateCategory } from "../controllers/category/update-category";
 
 const categoriesRouter = Router();
 
 categoriesRouter.route("/categories").post(createCategory).get(listCategories);
-categoriesRouter.route("/categories/:id").put(updateCategory);
+categoriesRouter.route("/categories/:id").put(updateCategory).get(getCategory);
 
 export { categoriesRouter };
--- src/express/routes/products.ts
+++ src/express/routes/products.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createProduct } from "../controllers/product/create-product";
+import { deleteProduct } from "../controllers/product/delete-product";
 import { updateProduct } from "../controllers/product/update-product";
 
 const productsRouter = Router();
 
 productsRouter.route("/products").post(createProduct);
-productsRouter.route("/products/:id").put(updateProduct);
+productsRouter.route("/products/:id").put(updateProduct).delete(deleteProduct);
 
 export { productsRouter };
